d1991f287d824bb70baa650cf409f8818b6743c2
Juanma <lorenzoperez.juanmaria@gmail.com>
2020-03-26 11:06:49 +0100

    feat (unsecure-sockets): Creación de sockets sin seguridad
    Se ha desarrollado un socket servidor y otro cliente que esta escuchando/retransmitiendo en localhost.

---
+++ src/unsecure_sockets/server.py
@@ -0,0 +1,17 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.bind(('localhost', 9992))
+
+server_socket.listen(5)
+
+while True:
+    
+    (clientsocket, address) = server_socket.accept()
+    print("Nueva conexion establecida", address)
+
+    
+    peticion = clientsocket.recv(1024)
+    print(peticion)
+    clientsocket.close()
